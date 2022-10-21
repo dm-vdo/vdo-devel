13cef14f1a2ca7934cdeea4da7e31d00d635589e
Matthew Sakai <msakai@redhat.com>
2022-10-21 17:40:07 -0500

    Add header documentation to event-count.h. [UDSUP-1]
    Pair: slegendr
    
    .../event-count.h
    .../event-count.c
    Moved usage documentation from event-count.c to event-count.h.
    
    [git-p4: depot-paths = "//eng/linux-vdo/": change = 157166]

--- src/c++/uds/src/uds/event-count.c
+++ src/c++/uds/src/uds/event-count.c
@@ -4,39 +4,6 @@
  */
 
 /*
- * An event count is a lock-free equivalent of a condition variable.
- *
- * Using an event count, a lock-free producer/consumer can wait for a state
- * change (adding an item to an empty queue, for example) without spinning or
- * falling back on the use of mutex-based locks. Signalling is cheap when
- * there are no waiters (a memory fence), and preparing to wait is also
- * inexpensive (an atomic add instruction).
- *
- * A lock-free producer should call event_count_broadcast() after any mutation
- * to the lock-free data structure that a consumer might be waiting on. The
- * consumers should poll for work like this:
- *
- *   for (;;) {
- *     // Fast path--no additional cost to consumer.
- *     if (lockfree_dequeue(&item))
- *       return item;
- *     // Two-step wait: get current token and poll state, either cancelling
- *     // the wait or waiting for the token to be signalled.
- *     event_token_t token = event_count_prepare(event_count);
- *     if (lockfree_dequeue(&item)) {
- *       event_count_cancel(event_count, token);
- *       return item;
- *     }
- *     event_count_wait(event_count, token, NULL);
- *     // State has changed, but must check condition again, so loop.
- *   }
- *
- * Once event_count_prepare() is called, the caller should neither sleep nor
- * perform long-running or blocking actions before passing the token to
- * event_count_cancel() or event_count_wait(). The implementation is optimized
- * for a short polling window, and will not perform well if there are
- * outstanding tokens that have been signalled but not waited upon.
- *
  * This event count implementation uses a posix semaphore for portability,
  * although a futex would be slightly superior to use and easy to substitute.
  * It is designed to make signalling as cheap as possible, since that is the
--- src/c++/uds/src/uds/event-count.h
+++ src/c++/uds/src/uds/event-count.h
@@ -9,6 +9,41 @@
 #include "time-utils.h"
 #include "type-defs.h"
 
+/*
+ * An event count is a lock-free equivalent of a condition variable.
+ *
+ * Using an event count, a lock-free producer/consumer can wait for a state
+ * change (adding an item to an empty queue, for example) without spinning or
+ * falling back on the use of mutex-based locks. Signalling is cheap when
+ * there are no waiters (a memory fence), and preparing to wait is also
+ * inexpensive (an atomic add instruction).
+ *
+ * A lock-free producer should call event_count_broadcast() after any mutation
+ * to the lock-free data structure that a consumer might be waiting on. The
+ * consumers should poll for work like this:
+ *
+ *   for (;;) {
+ *     // Fast path--no additional cost to consumer.
+ *     if (lockfree_dequeue(&item))
+ *       return item;
+ *     // Two-step wait: get current token and poll state, either cancelling
+ *     // the wait or waiting for the token to be signalled.
+ *     event_token_t token = event_count_prepare(event_count);
+ *     if (lockfree_dequeue(&item)) {
+ *       event_count_cancel(event_count, token);
+ *       return item;
+ *     }
+ *     event_count_wait(event_count, token, NULL);
+ *     // State has changed, but must check condition again, so loop.
+ *   }
+ *
+ * Once event_count_prepare() is called, the caller should neither sleep nor
+ * perform long-running or blocking actions before passing the token to
+ * event_count_cancel() or event_count_wait(). The implementation is optimized
+ * for a short polling window, and will not perform well if there are
+ * outstanding tokens that have been signalled but not waited upon.
+ */
+
 struct event_count;
 
 typedef unsigned int event_token_t;
