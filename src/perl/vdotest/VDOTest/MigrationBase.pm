fb48b3c22f6439fccd5b9841b14570c922700d60
Susan LeGendre-McGhee <slegendr@redhat.com>
2023-01-31 18:01:47 -0500

    Merge change 152796 to linux-vdo.
    Pair: Scliff 
    
    .../MigrationBase.pm
    .../UpgradeBase.pm
    152796: Refactor UpgradeBase and MigrationBase in preparation for new
      tests.
    
    [git-p4: depot-paths = "//eng/linux-vdo/": change = 157734]

--- src/perl/vdotest/VDOTest/MigrationBase.pm
+++ src/perl/vdotest/VDOTest/MigrationBase.pm
@@ -12,9 +12,7 @@ use English qw(-no_match_vars);
 use Log::Log4perl;
 
 use Permabit::Assertions qw(
-  assertEqualNumeric
-  assertGTNumeric
-  assertLTNumeric
+  assertMinMaxArgs
   assertNumArgs
 );
 use Permabit::Constants;
@@ -29,181 +27,15 @@ my $log = Log::Log4perl->get_logger(__PACKAGE__);
 our %PROPERTIES =
   (
    # @ple Audit VDO metadata
-   auditVDO           => 1,
+   auditVDO   => 1,
    # @ple Number of blocks to write
-   blockCount         => 33000,
+   blockCount => 33000,
    # @ple Use an upgrade device backed by iscsi, and don't use stripfua
-   deviceType         => "upgrade-iscsi-linear",
-   # @ple VDO Stats to compare after upgrade
-   _preMigrationStats => undef,
+   deviceType => "upgrade-iscsi-linear",
    # @ple Data to share between test methods
-   _testData          => undef,
+   _testData  => undef,
   );
 ##
-
-#############################################################################
-##
-sub establishStartingDevice {
-  my ($self) = assertNumArgs(1, @_);
-
-  my $device = $self->getDevice();
-  my $dataSize = $self->{blockCount} * $self->{blockSize};
-
-  my $initStats = $device->getVDOStats();
-  assertEqualNumeric(0, $initStats->{"data blocks used"},
-                     "Starting data blocks used should be zero");
-  assertEqualNumeric(0, $initStats->{"logical blocks used"},
-                     "Starting logical block used should be zero");
-
-  $self->{_testdata}
-    = [
-       $self->createSlice(
-                          blockCount => $self->{blockCount},
-                         ),
-       $self->createSlice(
-                          blockCount => $self->{blockCount} / 2,
-                          offset     => $self->{blockCount},
-                         ),
-       $self->createSlice(
-                          blockCount => $self->{blockCount} / 2,
-                          offset     => $self->{blockCount} * 3 / 2,
-                         ),
-      ];
-  $self->{_testdata}[0]->write(tag => "data", fsync => 1);
-  # Write half the blocks again, expecting complete dedupe.
-  $self->{_testdata}[1]->write(tag => "data", fsync => 1);
-
-  # If we have dedupe enabled, make sure we get exactly the dedupe we want.
-  my $preCompressStats = $device->getVDOStats();
-  if (!$self->{disableAlbireo}) {
-    assertEqualNumeric($self->{blockCount},
-                       $preCompressStats->{"data blocks used"},
-                       "Data blocks used should be block count");
-  }
-
-  # Write some compressible blocks
-  my $compression = $device->isVDOCompressionEnabled();
-  $device->enableCompression();
-  $self->{_testdata}[2]->write(tag => "data2", fsync => 1, compress => .9);
-  # Restore old compression status; then make sure compression happened.
-  if (!$compression) {
-    $device->disableCompression();
-  }
-  $self->{_preMigrationStats} = $device->getVDOStats();
-  assertGTNumeric($self->{_preMigrationStats}->{"compressed blocks written"}, 0,
-                  "There are some compressed blocks");
-  assertGTNumeric($self->{_preMigrationStats}->{"compressed fragments written"},
-                  0, "There are some compressed fragments");
-  # With 90% compressible data, we should at least save 80% of the space.
-  my $dataBlocksUsed = ($self->{_preMigrationStats}->{"data blocks used"}
-                        - $preCompressStats->{"data blocks used"});
-  assertLTNumeric($dataBlocksUsed, $self->{blockCount} / 5,
-                  "Block writes should be compressed");
-
-  # Don't check dedupe stats if we aren't running with deduplication.
-  if (!$self->{disableAlbireo}) {
-    assertEqualNumeric(0, $self->{_preMigrationStats}->{"dedupe advice timeouts"},
-                       "Dedupe advice timeouts should be zero");
-    assertEqualNumeric(($self->{blockCount} / 2),
-                       $self->{_preMigrationStats}->{"dedupe advice valid"},
-                       "Dedupe advice valid should be half the block count");
-    assertEqualNumeric(0, $self->{_preMigrationStats}->{"dedupe advice stale"},
-                       "Dedupe advice stale should be zero");
-  }
-}
-
-#############################################################################
-# Test basic read/write and dedupe capability after an upgrade.
-##
-sub verifyFinalState {
-  my ($self) = assertNumArgs(1, @_);
-
-  my $device = $self->getDevice();
-  my $postMigrationStats = $device->getVDOStats();
-  my @unchangedStats = (
-                         "data blocks used",
-                         "logical blocks used",
-                         "logical blocks",
-                         "physical blocks",
-                        );
-  foreach my $field (@unchangedStats) {
-    assertEqualNumeric($self->{_preMigrationStats}->{$field},
-                       $postMigrationStats->{$field},
-                       "Stats for $field should be the same");
-  }
-
-  # Verify all the data have not changed.
-  $self->{_testdata}[0]->verify();
-  $self->{_testdata}[1]->verify();
-  $self->{_testdata}[2]->verify();
-
-  # Overwrite the second half with the first half of data blocks.
-  # Data should dedupe and free up half the blocks.
-  my $blocksWritten = $self->{blockCount} / 2;
-  my $overwriteSlice
-    = $self->createSlice(
-                         blockCount => $blocksWritten,
-                         offset     => $self->{blockCount} / 2,
-                        );
-  $overwriteSlice->write(tag => "data", fsync => 1);
-
-  my $overwriteDedupeStats = $device->getVDOStats();
-  assertEqualNumeric(0, $overwriteDedupeStats->{"dedupe advice timeouts"},
-                     "Dedupe advice timeouts should be zero");
-  assertEqualNumeric($blocksWritten,
-                     $overwriteDedupeStats->{"dedupe advice valid"},
-                     "Dedupe advice valid should be block count");
-  assertEqualNumeric(0, $overwriteDedupeStats->{"dedupe advice stale"},
-                     "Dedupe advice stale should be zero");
-  assertEqualNumeric(($self->{_preMigrationStats}->{"data blocks used"}
-                      - $blocksWritten),
-                     $overwriteDedupeStats->{"data blocks used"},
-                     "Each overwritten block freed a used block");
-
-  # Reboot then continue to use the new VDO.
-  $self->rebootMachineForDevice($device);
-  $device->verifyModuleVersion();
-
-  # Grow the VDO volume by an arbitrary 60%, then read.
-  my $newPhysicalSize = int($self->{physicalSize} * 8 / 5);
-  $device->growPhysical($newPhysicalSize);
-  $overwriteSlice->verify();
-
-  # Enable compression and write compressible data.
-  $device->enableCompression();
-
-  my $compressSlice
-    = $self->createSlice(
-                         blockCount => $self->{blockCount},
-                         offset     => $self->{blockCount} * 3,
-                        );
-  $compressSlice->write(tag => "data", fsync => 1, compress => .9);
-  $compressSlice->verify();
-
-  my $compressionStats = $device->getVDOStats();
-  # With 90% compressible data, we should at least save 80% of the space.
-  my $dataBlocksUsed = ($compressionStats->{"data blocks used"}
-                        - $overwriteDedupeStats->{"data blocks used"});
-  assertLTNumeric($dataBlocksUsed, $self->{blockCount} / 5,
-                  "Block writes should be compressed");
-  assertGTNumeric($compressionStats->{"compressed blocks written"}, 0,
-                  "There are some compressed blocks");
-  assertGTNumeric($compressionStats->{"compressed fragments written"}, 0,
-                  "There are some compressed fragments");
-
-  # Grow logical by an arbitrary 60%, then use it.
-  my $offTheEndSlice
-    = $self->createSlice(
-                         blockCount => $self->{blockCount},
-                         offset     => $self->{logicalSize} / $KB / 4,
-                        );
-
-  my $newLogicalBytes = ceilMultiple(int($self->{logicalSize} * 8 / 5),
-                                     $DEFAULT_BLOCK_SIZE);
-  $device->growLogical($newLogicalBytes);
-  $offTheEndSlice->write(tag => "end", fsync => 1);
-  $offTheEndSlice->verify();
-}
 
 #############################################################################
 # Move the VDO device to a new scenario.
@@ -214,6 +46,39 @@ sub switchToIntermediateScenario {
   $device->stop();
   $device->switchToScenario($scenario);
   $device->start();
+}
+
+#############################################################################
+# Test basic read/write and dedupe capability after a series of upgrades.
+#
+# @oparam  dontVerify  don't run the final verification step
+##
+sub _runUpgradeTest {
+  my ($self, $dontVerify) = assertMinMaxArgs([0], 1, 2, @_);
+  my @upgradeList = @{$self->{intermediateVersions}};
+  push(@upgradeList, "head");
+
+  my $upgrades = join(" to ", @upgradeList);
+  $log->info("Test upgrading $self->{initialScenario}{version} to $upgrades");
+
+  $self->establishStartingDevice();
+  my $device = $self->getDevice();
+  foreach my $intermediateVersion (@upgradeList) {
+    if ($device->needsExplicitUpgrade($intermediateVersion)) {
+      $log->info("Upgrading to $intermediateVersion explicitly");
+      $self->doUpgrade($intermediateVersion);
+    } else {
+      $log->info("Upgrading to $intermediateVersion implicitly");
+      $self->_switchToIntermediateVersion($intermediateVersion);
+    }
+  }
+  if (!$dontVerify) {
+    $self->verifyFinalState();
+  }
+
+  $log->info("Returning to $self->{initialScenario}{version}");
+  $self->_switchToIntermediateVersion($self->{initialScenario}{version});
+  # XXX  We should check that the old version still works.
 }
 
 1;
