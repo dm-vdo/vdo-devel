kernelorg-mirror/linux_kernel_git_snitzer_linux

dm vdo memory-alloc: rename UDS_FORGET to uds_forget
Signed-off-by: Mike Snitzer <snitzer@kernel.org>

drivers/md/dm-vdo/block-map.c
@@ -1332,7 +1332,7 @@ int vdo_invalidate_page_cache(struct vdo_page_cache *cache)
 	}
 
 	/* Reset the page map by re-allocating it. */
-	vdo_hash_map_free(UDS_FORGET(cache->page_map));
+	vdo_hash_map_free(uds_forget(cache->page_map));
 	return vdo_hash_map_create(HASH_MAP_TYPE_INT, cache->page_count, &cache->page_map);
 }
 
@@ -2496,7 +2496,7 @@ static void replace_forest(struct block_map *map)
 	if (map->next_forest != NULL) {
 		if (map->forest != NULL)
 			deforest(map->forest, map->forest->segments);
-		map->forest = UDS_FORGET(map->next_forest);
+		map->forest = uds_forget(map->next_forest);
 	}
 
 	map->entry_count = map->next_entry_count;
@@ -2512,7 +2512,7 @@ static void finish_cursor(struct cursor *cursor)
 	struct cursors *cursors = cursor->parent;
 	struct vdo_completion *parent = cursors->parent;
 
-	return_vio_to_pool(cursors->pool, UDS_FORGET(cursor->vio));
+	return_vio_to_pool(cursors->pool, uds_forget(cursor->vio));
 	if (--cursors->active_roots > 0)
 		return;
 
@@ -2858,19 +2858,19 @@ static void uninitialize_block_map_zone(struct block_map_zone *zone)
 {
 	struct vdo_page_cache *cache = &zone->page_cache;
 
-	UDS_FREE(UDS_FORGET(zone->dirty_lists));
-	free_vio_pool(UDS_FORGET(zone->vio_pool));
-	vdo_hash_map_free(UDS_FORGET(zone->loading_pages));
+	UDS_FREE(uds_forget(zone->dirty_lists));
+	free_vio_pool(uds_forget(zone->vio_pool));
+	vdo_hash_map_free(uds_forget(zone->loading_pages));
 	if (cache->infos != NULL) {
 		struct page_info *info;
 
 		for (info = cache->infos; info < cache->infos + cache->page_count; ++info)
-			free_vio(UDS_FORGET(info->vio));
+			free_vio(uds_forget(info->vio));
 	}
 
-	vdo_hash_map_free(UDS_FORGET(cache->page_map));
-	UDS_FREE(UDS_FORGET(cache->infos));
-	UDS_FREE(UDS_FORGET(cache->pages));
+	vdo_hash_map_free(uds_forget(cache->page_map));
+	UDS_FREE(uds_forget(cache->infos));
+	UDS_FREE(uds_forget(cache->pages));
 }
 
 void vdo_free_block_map(struct block_map *map)
@@ -2885,8 +2885,8 @@ void vdo_free_block_map(struct block_map *map)
 
 	vdo_abandon_block_map_growth(map);
 	if (map->forest != NULL)
-		deforest(UDS_FORGET(map->forest), 0);
-	UDS_FREE(UDS_FORGET(map->action_manager));
+		deforest(uds_forget(map->forest), 0);
+	UDS_FREE(uds_forget(map->action_manager));
 	UDS_FREE(map);
 }
 
@@ -3103,7 +3103,7 @@ void vdo_grow_block_map(struct block_map *map, struct vdo_completion *parent)
 
 void vdo_abandon_block_map_growth(struct block_map *map)
 {
-	struct forest *forest = UDS_FORGET(map->next_forest);
+	struct forest *forest = uds_forget(map->next_forest);
 
 	if (forest != NULL)
 		deforest(forest, forest->segments - 1);

drivers/md/dm-vdo/dedupe.c
@@ -686,7 +686,7 @@ static void unlock_duplicate_pbn(struct vdo_completion *completion)
 
 	vdo_release_physical_zone_pbn_lock(agent->duplicate.zone,
 					   agent->duplicate.pbn,
-					   UDS_FORGET(lock->duplicate_lock));
+					   uds_forget(lock->duplicate_lock));
 	if (lock->state == VDO_HASH_LOCK_BYPASSING) {
 		complete_data_vio(completion);
 		return;
@@ -880,7 +880,7 @@ static int __must_check acquire_lock(struct hash_zone *zone,
 	result = vdo_hash_map_put(zone->hash_lock_map, &new_lock->hash, new_lock,
 				  (replace_lock != NULL), (void **) &lock);
 	if (result != VDO_SUCCESS) {
-		return_hash_lock_to_pool(zone, UDS_FORGET(new_lock));
+		return_hash_lock_to_pool(zone, uds_forget(new_lock));
 		return result;
 	}
 
@@ -898,7 +898,7 @@ static int __must_check acquire_lock(struct hash_zone *zone,
 		lock->registered = true;
 	} else {
 		/* There's already a lock for the hash, so we don't need the borrowed lock. */
-		return_hash_lock_to_pool(zone, UDS_FORGET(new_lock));
+		return_hash_lock_to_pool(zone, uds_forget(new_lock));
 	}
 
 	*lock_ptr = lock;
@@ -1952,7 +1952,7 @@ static void transfer_allocation_lock(struct data_vio *data_vio)
 	 * Since the lock is being transferred, the holder count doesn't change (and isn't even
 	 * safe to examine on this thread).
 	 */
-	hash_lock->duplicate_lock = UDS_FORGET(allocation->lock);
+	hash_lock->duplicate_lock = uds_forget(allocation->lock);
 }
 
 /**
@@ -2227,7 +2227,7 @@ static int initialize_index(struct vdo *vdo, struct hash_zones *zones)
 
 	result = vdo_make_thread(vdo, vdo->thread_config.dedupe_thread, &uds_queue_type, 1, NULL);
 	if (result != VDO_SUCCESS) {
-		uds_destroy_index_session(UDS_FORGET(zones->index_session));
+		uds_destroy_index_session(uds_forget(zones->index_session));
 		uds_log_error("UDS index queue initialization failed (%d)", result);
 		return result;
 	}
@@ -2486,7 +2486,7 @@ void vdo_finish_dedupe_index(struct hash_zones *zones)
 	if (zones == NULL)
 		return;
 
-	uds_destroy_index_session(UDS_FORGET(zones->index_session));
+	uds_destroy_index_session(uds_forget(zones->index_session));
 }
 
 /**
@@ -2500,14 +2500,14 @@ void vdo_free_hash_zones(struct hash_zones *zones)
 	if (zones == NULL)
 		return;
 
-	UDS_FREE(UDS_FORGET(zones->manager));
+	UDS_FREE(uds_forget(zones->manager));
 
 	for (i = 0; i < zones->zone_count; i++) {
 		struct hash_zone *zone = &zones->zones[i];
 
-		uds_free_funnel_queue(UDS_FORGET(zone->timed_out_complete));
-		vdo_hash_map_free(UDS_FORGET(zone->hash_lock_map));
-		UDS_FREE(UDS_FORGET(zone->lock_array));
+		uds_free_funnel_queue(uds_forget(zone->timed_out_complete));
+		vdo_hash_map_free(uds_forget(zone->hash_lock_map));
+		UDS_FREE(uds_forget(zone->lock_array));
 	}
 
 	if (zones->index_session != NULL)

dm-vdo/vdo-devel

src/c++/vdo/base/block-map.c
@@ -1338,7 +1338,7 @@ int vdo_invalidate_page_cache(struct vdo_page_cache *cache)
 	}
 
 	/* Reset the page map by re-allocating it. */
-	vdo_free_int_map(UDS_FORGET(cache->page_map));
+	vdo_free_int_map(uds_forget(cache->page_map));
 	return vdo_make_int_map(cache->page_count, 0, &cache->page_map);
 }
 
@@ -2505,7 +2505,7 @@ static void replace_forest(struct block_map *map)
 	if (map->next_forest != NULL) {
 		if (map->forest != NULL)
 			deforest(map->forest, map->forest->segments);
-		map->forest = UDS_FORGET(map->next_forest);
+		map->forest = uds_forget(map->next_forest);
 	}
 
 	map->entry_count = map->next_entry_count;
@@ -2521,7 +2521,7 @@ static void finish_cursor(struct cursor *cursor)
 	struct cursors *cursors = cursor->parent;
 	struct vdo_completion *parent = cursors->parent;
 
-	return_vio_to_pool(cursors->pool, UDS_FORGET(cursor->vio));
+	return_vio_to_pool(cursors->pool, uds_forget(cursor->vio));
 	if (--cursors->active_roots > 0)
 		return;
 
@@ -2860,19 +2860,19 @@ static void uninitialize_block_map_zone(struct block_map_zone *zone)
 {
 	struct vdo_page_cache *cache = &zone->page_cache;
 
-	UDS_FREE(UDS_FORGET(zone->dirty_lists));
-	free_vio_pool(UDS_FORGET(zone->vio_pool));
-	vdo_free_int_map(UDS_FORGET(zone->loading_pages));
+	UDS_FREE(uds_forget(zone->dirty_lists));
+	free_vio_pool(uds_forget(zone->vio_pool));
+	vdo_free_int_map(uds_forget(zone->loading_pages));
 	if (cache->infos != NULL) {
 		struct page_info *info;
 
 		for (info = cache->infos; info < cache->infos + cache->page_count; ++info)
-			free_vio(UDS_FORGET(info->vio));
+			free_vio(uds_forget(info->vio));
 	}
 
-	vdo_free_int_map(UDS_FORGET(cache->page_map));
-	UDS_FREE(UDS_FORGET(cache->infos));
-	UDS_FREE(UDS_FORGET(cache->pages));
+	vdo_free_int_map(uds_forget(cache->page_map));
+	UDS_FREE(uds_forget(cache->infos));
+	UDS_FREE(uds_forget(cache->pages));
 }
 
 void vdo_free_block_map(struct block_map *map)
@@ -2887,8 +2887,8 @@ void vdo_free_block_map(struct block_map *map)
 
 	vdo_abandon_block_map_growth(map);
 	if (map->forest != NULL)
-		deforest(UDS_FORGET(map->forest), 0);
-	UDS_FREE(UDS_FORGET(map->action_manager));
+		deforest(uds_forget(map->forest), 0);
+	UDS_FREE(uds_forget(map->action_manager));
 	UDS_FREE(map);
 }
 
@@ -3105,7 +3105,7 @@ void vdo_grow_block_map(struct block_map *map, struct vdo_completion *parent)
 
 void vdo_abandon_block_map_growth(struct block_map *map)
 {
-	struct forest *forest = UDS_FORGET(map->next_forest);
+	struct forest *forest = uds_forget(map->next_forest);
 
 	if (forest != NULL)
 		deforest(forest, forest->segments - 1);

src/c++/vdo/base/dedupe.c
@@ -690,7 +690,7 @@ static void unlock_duplicate_pbn(struct vdo_completion *completion)
 
 	vdo_release_physical_zone_pbn_lock(agent->duplicate.zone,
 					   agent->duplicate.pbn,
-					   UDS_FORGET(lock->duplicate_lock));
+					   uds_forget(lock->duplicate_lock));
 	if (lock->state == VDO_HASH_LOCK_BYPASSING) {
 		complete_data_vio(completion);
 		return;
@@ -891,7 +891,7 @@ static int __must_check acquire_lock(struct hash_zone *zone,
 				     (replace_lock != NULL),
 				     (void **) &lock);
 	if (result != VDO_SUCCESS) {
-		return_hash_lock_to_pool(zone, UDS_FORGET(new_lock));
+		return_hash_lock_to_pool(zone, uds_forget(new_lock));
 		return result;
 	}
 
@@ -909,7 +909,7 @@ static int __must_check acquire_lock(struct hash_zone *zone,
 		lock->registered = true;
 	} else {
 		/* There's already a lock for the hash, so we don't need the borrowed lock. */
-		return_hash_lock_to_pool(zone, UDS_FORGET(new_lock));
+		return_hash_lock_to_pool(zone, uds_forget(new_lock));
 	}
 
 	*lock_ptr = lock;
@@ -1979,7 +1979,7 @@ static void transfer_allocation_lock(struct data_vio *data_vio)
 	 * Since the lock is being transferred, the holder count doesn't change (and isn't even
 	 * safe to examine on this thread).
 	 */
-	hash_lock->duplicate_lock = UDS_FORGET(allocation->lock);
+	hash_lock->duplicate_lock = uds_forget(allocation->lock);
 }
 
 /**
@@ -2274,7 +2274,7 @@ static int initialize_index(struct vdo *vdo, struct hash_zones *zones)
 
 	result = vdo_make_thread(vdo, vdo->thread_config.dedupe_thread, &uds_queue_type, 1, NULL);
 	if (result != VDO_SUCCESS) {
-		uds_destroy_index_session(UDS_FORGET(zones->index_session));
+		uds_destroy_index_session(uds_forget(zones->index_session));
 		uds_log_error("UDS index queue initialization failed (%d)", result);
 		return result;
 	}
@@ -2536,7 +2536,7 @@ void vdo_finish_dedupe_index(struct hash_zones *zones)
 	if (zones == NULL)
 		return;
 
-	uds_destroy_index_session(UDS_FORGET(zones->index_session));
+	uds_destroy_index_session(uds_forget(zones->index_session));
 }
 
 /**
@@ -2550,14 +2550,14 @@ void vdo_free_hash_zones(struct hash_zones *zones)
 	if (zones == NULL)
 		return;
 
-	UDS_FREE(UDS_FORGET(zones->manager));
+	UDS_FREE(uds_forget(zones->manager));
 
 	for (i = 0; i < zones->zone_count; i++) {
 		struct hash_zone *zone = &zones->zones[i];
 
-		uds_free_funnel_queue(UDS_FORGET(zone->timed_out_complete));
-		vdo_free_pointer_map(UDS_FORGET(zone->hash_lock_map));
-		UDS_FREE(UDS_FORGET(zone->lock_array));
+		uds_free_funnel_queue(uds_forget(zone->timed_out_complete));
+		vdo_free_pointer_map(uds_forget(zone->hash_lock_map));
+		UDS_FREE(uds_forget(zone->lock_array));
 	}
 
 	if (zones->index_session != NULL)