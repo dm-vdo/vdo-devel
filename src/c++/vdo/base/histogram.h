a8f324c6517b74bea9317e633b6b301e7306ed94
Ken Raeburn <raeburn@redhat.com>
2023-11-06 19:53:49 -0500

    dm vdo: function decl/call reflowing: histogram.[ch] vdo-histograms.c
    Mechanical rewrapping (with minor manual patching) of function
    declarations, definition headers, and function calls to better
    approximate device-mapper style, using 90-column soft line-length
    limit.
    
    Signed-off-by: Ken Raeburn <raeburn@redhat.com>

--- src/c++/vdo/base/histogram.h
+++ src/c++/vdo/base/histogram.h
@@ -8,28 +8,21 @@
 
 #include <linux/types.h>
 
-struct histogram *make_linear_histogram(struct kobject *parent,
-					const char *name,
+struct histogram *make_linear_histogram(struct kobject *parent, const char *name,
 					const char *init_label,
-					const char *counted_items,
-					const char *metric,
-					const char *sample_units,
-					int size);
+					const char *counted_items, const char *metric,
+					const char *sample_units, int size);
 
-struct histogram *make_logarithmic_histogram(struct kobject *parent,
-					     const char *name,
+struct histogram *make_logarithmic_histogram(struct kobject *parent, const char *name,
 					     const char *init_label,
 					     const char *counted_items,
 					     const char *metric,
-					     const char *sample_units,
-					     int log_size);
+					     const char *sample_units, int log_size);
 
-struct histogram *make_logarithmic_jiffies_histogram(struct kobject *parent,
-						     const char *name,
+struct histogram *make_logarithmic_jiffies_histogram(struct kobject *parent, const char *name,
 						     const char *init_label,
 						     const char *counted_items,
-						     const char *metric,
-						     int log_size);
+						     const char *metric, int log_size);
 
 void enter_histogram_sample(struct histogram *h, u64 sample);
 
