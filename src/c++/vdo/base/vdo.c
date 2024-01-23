91b6f88ff820a3ce884bcf27235896320777466b
Mike Snitzer <snitzer@kernel.org>
2024-01-23 10:35:04 -0600

    dm vdo: fix sparse 'warning: Using plain integer as NULL pointer'
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    Signed-off-by: Susan LeGendre-McGhee <slegendr@redhat.com>

--- src/c++/vdo/base/vdo.c
+++ src/c++/vdo/base/vdo.c
@@ -934,7 +934,8 @@ int vdo_synchronous_flush(struct vdo *vdo)
 	bio_set_dev(&bio, vdo_get_backing_device(vdo));
 	bio.bi_opf = REQ_OP_WRITE | REQ_PREFLUSH;
 #else
-	bio_init(&bio, vdo_get_backing_device(vdo), 0, 0, REQ_OP_WRITE | REQ_PREFLUSH);
+	bio_init(&bio, vdo_get_backing_device(vdo), NULL, 0,
+		 REQ_OP_WRITE | REQ_PREFLUSH);
 #endif
 	submit_bio_wait(&bio);
 	result = blk_status_to_errno(bio.bi_status);
