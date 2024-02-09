b4a4bf9538884f8c50a63f26cb888e6dd909753b
Mike Snitzer <snitzer@kernel.org>
2024-02-09 12:08:09 -0600

    dm vdo thread-utils: further cleanup of thread functions
    Change thread function prefix from "uds_" to "vdo_" and fix
    vdo_join_threads() to return void.
    
    Signed-off-by: Mike Snitzer <snitzer@kernel.org>
    Signed-off-by: Matthew Sakai <msakai@redhat.com>

--- src/c++/uds/src/uds/thread-utils.h
+++ src/c++/uds/src/uds/thread-utils.h
@@ -25,7 +25,7 @@
 #include "time-utils.h"
 #endif
 
-/* Thread and synchronization utilities for UDS */
+/* Thread and synchronization utilities */
 
 #ifdef __KERNEL__
 struct thread;
@@ -59,12 +59,11 @@ unsigned int num_online_cpus(void);
 pid_t __must_check uds_get_thread_id(void);
 #endif
 
-int __must_check uds_create_thread(void (*thread_function)(void *), void *thread_data,
+int __must_check vdo_create_thread(void (*thread_function)(void *), void *thread_data,
 				   const char *name, struct thread **new_thread);
+int vdo_join_threads(struct thread *thread);
 
-void uds_perform_once(atomic_t *once_state, void (*function) (void));
-
-int uds_join_threads(struct thread *thread);
+void vdo_perform_once(atomic_t *once_state, void (*function) (void));
 #ifdef __KERNEL__
 #ifdef TEST_INTERNAL
 
