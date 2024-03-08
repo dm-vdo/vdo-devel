7691c142b1341965d11de9b932dee98bb9cdb884
Chung Chung <cchung@redhat.com>
2024-03-08 17:33:17 -0500

    dm-vdo: Add code to adapt kernel next block device changes
    In linux next kernel, blkdev_get_by_dev has been redefined and
    blkdev_put is obsolete. Update getTestIndexNames.c to adapt the
    kernel changes by adding VDO_USE_ALTERNATE_3 and rearranging
    VDO_USE_ALTERNATE marco.
    
    Signed-off-by: Chung Chung <cchung@redhat.com>

--- src/c++/uds/kernelLinux/tests/getTestIndexNames.c
+++ src/c++/uds/kernelLinux/tests/getTestIndexNames.c
@@ -10,16 +10,21 @@
 
 #undef VDO_USE_ALTERNATE
 #undef VDO_USE_ALTERNATE_2
+#undef VDO_USE_ALTERNATE_3
 #ifdef RHEL_RELEASE_CODE
 #define VDO_USE_ALTERNATE
-#if (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(9, 4))
 #define VDO_USE_ALTERNATE_2
+#if (RHEL_RELEASE_CODE < RHEL_RELEASE_VERSION(9, 4))
+#define VDO_USE_ALTERNATE_3
 #endif
 #else /* !RHEL_RELEASE_CODE */
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(6,7,0))
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6,9,0))
 #define VDO_USE_ALTERNATE
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(6,5,0))
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6,7,0))
 #define VDO_USE_ALTERNATE_2
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(6,5,0))
+#define VDO_USE_ALTERNATE_3
+#endif
 #endif
 #endif
 #endif /* !RHEL_RELEASE_CODE */
@@ -30,21 +35,32 @@ enum { BLK_FMODE = FMODE_READ | FMODE_WRITE };
 static struct block_device *get_device_from_name(const char *name)
 {
 	struct block_device *bdev;
+
 #ifdef VDO_USE_ALTERNATE
 #ifdef VDO_USE_ALTERNATE_2
+#ifdef VDO_USE_ALTERNATE_3
 
 	bdev = blkdev_get_by_path(name, BLK_FMODE, NULL);
 #else
 	const struct blk_holder_ops hops = { NULL };
 
 	bdev = blkdev_get_by_path(name, BLK_FMODE, NULL, &hops);
-#endif /* VDO_USE_ALTERNATE_2 */
+#endif /* VDO_USE_ALTERNATE_3 */
 #else
 	const struct blk_holder_ops hops = { NULL };
 	struct bdev_handle *bdev_handle;
 
 	bdev_handle = bdev_open_by_path(name, BLK_FMODE, NULL, &hops);
 	bdev = bdev_handle->bdev;
+#endif /* VDO_USE_ALTERNATE_2 */
+#else
+	const struct blk_holder_ops hops = { NULL };
+	struct file *file_handle;
+	
+	file_handle = bdev_file_open_by_path(name, BLK_FMODE, NULL, &hops);
+	bdev = file_bdev(file_handle);
+	get_device(&bdev->bd_device);
+	fput(file_handle);
 #endif /* VDO_USE_ALTERNATE */
 
 	if (IS_ERR(bdev)) {
@@ -74,20 +90,24 @@ struct block_device **getTestMultiBlockDevices(void)
 /**********************************************************************/
 void putTestBlockDevice(struct block_device *bdev)
 {
-#ifndef VDO_USE_ALTERNATE
+#if defined(VDO_USE_ALTERNATE) && !defined(VDO_USE_ALTERNATE_2)
 	struct bdev_handle bdev_handle = { .bdev = bdev, };
 
-#endif /* VDO_USE_ALTERNATE */
+#endif /* VDO_USE_ALTERNATE && VDO_USE_ALTERNATE_2 */
 	if (bdev == NULL)
 		return;
 
 #ifdef VDO_USE_ALTERNATE
 #ifdef VDO_USE_ALTERNATE_2
+#ifdef VDO_USE_ALTERNATE_3
 	blkdev_put(bdev, BLK_FMODE);
 #else
 	blkdev_put(bdev, NULL);
-#endif /* VDO_USE_ALTERNATE_2 */
+#endif /* VDO_USE_ALTERNATE_3 */
 #else
 	bdev_release(&bdev_handle);
+#endif /* VDO_USE_ALTERNATE_2 */
+#else
+	put_device(&bdev->bd_device);
 #endif /* VDO_USE_ALTERNATE */
 }
