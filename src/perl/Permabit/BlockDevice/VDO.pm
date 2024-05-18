a407b48b967a423eef3b170e1589055499bb3e3c
Matthew Sakai <msakai@redhat.com>
2024-05-17 21:59:21 -0400

    dm vdo user: make vdoFillIndex read the VDO geometry
    Update vdoFillIndex to read the VDO geometry block to find the
    index parameters instead of requiring them as arguments. This
    simplifies the interface for vdoFillIndex, and it also removes
    the dependency on vdoDumpConfig, which was previously used to
    read the geometry parameters.
    
    Also fixed a minor bug where the VDO's bio_offset was not taken
    into account. This could only affect VDOs with a converted index.
    
    Signed-off-by: Matthew Sakai <msakai@redhat.com>

--- src/perl/Permabit/BlockDevice/VDO.pm
+++ src/perl/Permabit/BlockDevice/VDO.pm
@@ -307,23 +307,13 @@ sub dumpConfig {
 ########################################################################
 # Fill the index with synthetic records for steady state testing.
 #
-# @param  config        the index configuration from dumpConfig()
-# @oparam forceRebuild  If true, setup index to rebuild on next load
+# @oparam forceRebuild  If true, set up index to rebuild on next load
 ##
 sub fillIndex {
-  my ($self, $config, $forceRebuild) = assertMinMaxArgs([0], 2, 3, @_);
+  my ($self, $forceRebuild) = assertMinMaxArgs([0], 1, 2, @_);
 
-  my $nonce = $config->{Nonce};
-  my $offset = $config->{IndexRegion} * 4096;
-  my $sparse = $config->{IndexConfig}->{sparse};
   my $path = $self->getVDOStoragePath();
-
-  my $cmd = join(" ", $self->findBinary("vdoFillIndex"),
-                 "--nonce=$nonce", "--offset=$offset",
-                 (($sparse eq "true") ? "--sparse" : ()));
-  if (defined($self->{memorySize})) {
-    $cmd .= " --memory-size=$self->{memorySize}";
-  }
+  my $cmd = $self->findBinary("vdoFillIndex");
   if ($forceRebuild) {
     $cmd .= " --force-rebuild";
   }
@@ -352,8 +342,7 @@ sub postActivate {
   if (!$self->{disableAlbireo} && $self->{albFill}) {
     $self->waitForIndex();
     $self->{albFill} = 0;
-    my $config = $self->dumpConfig();
-    $self->fillIndex($config);
+    $self->fillIndex();
   }
 
   # In testing, we can try out different queue priorities
