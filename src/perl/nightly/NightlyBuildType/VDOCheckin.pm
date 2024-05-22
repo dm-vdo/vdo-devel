c8ecfb2eb4eaadf52941eede5077b212fd3d409f
Chung Chung <cchung@redhat.com>
2024-05-22 13:33:46 -0400

    dm vdo: Disable FEDORA38 testing and replace head with FEDORA39.
    Diable FEDORA38 from testing and replace any build reference
    from FEDORA38 to FEDORA39.
    
    Signed-off-by: Chung Chung <cchung@redhat.com>

--- src/perl/nightly/NightlyBuildType/VDOCheckin.pm
+++ src/perl/nightly/NightlyBuildType/VDOCheckin.pm
@@ -50,7 +50,7 @@ my $SUITE_PROPERTIES = {
 ##
 sub getSuitesImplementation {
   my ($self) = assertNumArgs(1, @_);
-  return generateTestSuites($SUITE_PROPERTIES, ["FEDORA38"]);
+  return generateTestSuites($SUITE_PROPERTIES, ["FEDORA39"]);
 }
 
 1;
