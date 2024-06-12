ed33681ee2060a1a212df3bb9899fc504f4b33bc
Bruce Johnston <bjohnsto@redhat.com>
2024-06-12 16:25:53 -0400

    Fix the DMTest/Separated.pm test.
    In order to get Separated.pm to work, we will create a single
    test machine using DMTest/Grouped.pm. This will allow us to run
    the "dmtest list" command in order to get the set of tests
    we want to run. Once we have the list, we can create a suite of
    tests; one DMTest/Grouped.pm instance for each item in the list.
    
    We also want to update dmtest.suites to make sure the Full suite
    uses just vdo tests, and not all tests in dmtest.
    
    Signed-off-by: Bruce Johnston <bjohnsto@redhat.com>

--- src/perl/dmtest/DMTest/Separated.pm
+++ src/perl/dmtest/DMTest/Separated.pm
@@ -13,6 +13,7 @@ use Log::Log4perl;
 
 use Permabit::Assertions qw(assertNumArgs);
 use Permabit::SystemUtils qw(assertSystem);
+use Permabit::UserMachine;
 use Permabit::Utils qw(makeFullPath);
 
 use base qw(DMTest::Grouped);
@@ -24,22 +25,28 @@ my $log = Log::Log4perl->get_logger(__PACKAGE__);
 sub suite {
   my ($package) = assertNumArgs(1, @_);
 
+  my @testNames = ();
   my $options = $package->makeDummyTest();
-  my $testList;
-  if (defined($options->{dmtestName})
-      && ($options->{dmtestName} ne "")
-      && ($options->{dmtestName} !~ /[\[\]{}*?]/)) {
-    # If unitTestName names isn't a pattern but appears to name exactly one
-    # test, just use it. If the name is bad, we'll find out soon enough.
-    $testList = $options->{dmtestName};
-  } else {
-    # TODO: Get all tests from dmtest
-    $testList = "vdo";
-  }
 
+  eval {
+    # Reserve a machine so we can look up the test names
+    $options->{rsvpMsg} = "Making $package suite";
+    $options->reserveHostGroup("client");
+    $options->set_up();
+    my $dmtestList = $options->listTests($options->{dmtestName});
+    my $hashes = $options->treeToHashes($dmtestList);
+    @testNames = $options->hashesToTests($hashes, "");
+  };
+  my $eval_error = $EVAL_ERROR;
+  # Always clean up
+  $options->tear_down();
+  $options->getRSVPer()->closeRSVP($package);
+  if ($eval_error) {
+    die($eval_error);
+  }
   # Now it is a simple matter of making and returning the suite
   my $suite = Test::Unit::TestSuite->empty_new($package);
-  foreach my $testName (sort(split(/\s+/, $testList))) {
+  foreach my $testName ( @testNames ) {
     my $test
       = $package->make_test_from_coderef(\&DMTest::Grouped::testRunner,
                                          "${package}::$testName");
