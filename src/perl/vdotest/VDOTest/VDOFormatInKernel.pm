42dc66b016314e9cb1498c8bfbf6f49a8fe8396b
Bruce Johnston <bjohnsto@redhat.com>
2024-11-19 14:35:17 -0500

    dm vdo test: add perl test for in kernel formatting
    Add a new perl test which is a copy of VDOFormat.pm that has been
    modified to work with in kernel formatting.
    
    A separate test makes sense since some of the error messages are
    different between vdoformat and the kernel and some parameters behave
    differently when passed into the kernel.
    
    Signed-off-by: Bruce Johnston <bjohnsto@redhat.com>

---
+++ src/perl/vdotest/VDOTest/VDOFormatInKernel.pm
@@ -0,0 +1,113 @@
+##
+# Test the vdoFormat in the kernel code
+#
+# $Id$
+##
+package VDOTest::VDOFormatInKernel;
+
+use strict;
+use warnings FATAL => qw(all);
+use English qw(-no_match_vars);
+use Log::Log4perl;
+use Math::BigInt;
+
+use Permabit::Assertions qw(
+  assertDefined
+  assertEvalErrorMatches
+  assertNumArgs
+  assertRegexpMatches
+  assertTrue
+);
+use Permabit::Constants;
+
+use base qw(VDOTest);
+
+my $log = Log::Log4perl->get_logger(__PACKAGE__);
+
+our %PROPERTIES
+  = (
+     # @ple set up a linear device
+     deviceType     => "linear",
+     # @ple format in kernel
+     formatInKernel => 1,
+     # @ple Use a size that will work for the full range of slab sizes
+     physicalSize   => 50 * $GB,
+    );
+
+########################################################################
+# @inherit
+##
+sub set_up {
+  my ($self) = assertNumArgs(1, @_);
+  $self->SUPER::set_up();
+  $self->createTestDevice("vdo", setupOnCreation => 0);
+}
+
+########################################################################
+##
+sub _tryIllegal {
+  my ($self, $paramName, $value, $error, $kernelError) = assertNumArgs(5, @_);
+
+  my $vdoDevice = $self->getDevice();
+  my $machine = $vdoDevice->getMachine();
+
+  my $preformatCursor = $machine->getKernelJournalCursor();
+  eval {
+    $vdoDevice->formatVDO({ $paramName => $value });
+  };
+  assertEvalErrorMatches(qr/ reload ioctl on .* failed:.*\Q$error\E/);
+  assertTrue($machine->searchKernelJournalSince($preformatCursor, $kernelError));
+}
+
+########################################################################
+# Most of these tests are the same as from the VDOFormat test, but we are
+# testing the format in the kernel code instead of the vdoformat application,
+# so some messages are different.
+##
+sub testOptions {
+  my ($self) = assertNumArgs(1, @_);
+  my $maxUInt = ~0;
+  my $oneMoreThanMax = "18446744073709551616";
+  my $vdoDevice = $self->getDevice();
+
+  # Validated through the VDO kernel code
+  my $kernelError = "invalid slab size";
+  my $dmsetupError = "Invalid argument";
+  $self->_tryIllegal("slabBits",              -2, $dmsetupError, $kernelError);
+  $self->_tryIllegal("slabBits",               3, $dmsetupError, $kernelError);
+  $self->_tryIllegal("slabBits",              25, $dmsetupError, $kernelError);
+  $self->_tryIllegal("slabBits",  ($maxUInt - 1), $dmsetupError, $kernelError);
+  $self->_tryIllegal("slabBits",        $maxUInt, $dmsetupError, $kernelError);
+  $self->_tryIllegal("slabBits", $oneMoreThanMax, $dmsetupError, $kernelError);
+
+  # Validated through the dmsetup code
+  $kernelError = "zero-length target";
+  $self->_tryIllegal("logicalSize", 0, $dmsetupError, $kernelError);
+
+  # Validated through the VDO kernel code
+  $kernelError = "must be a multiple of";
+  $self->_tryIllegal("logicalSize",            1024, $dmsetupError, $kernelError);
+  $self->_tryIllegal("logicalSize", ((4 * $TB) + 512), $dmsetupError, $kernelError);
+
+  # Validated through the VDO kernel code
+  $kernelError = "exceeds the maximum";
+  my $bigNum = Math::BigInt->new((4 * $PB) + 512);
+  $self->_tryIllegal("logicalSize", $bigNum, $dmsetupError, $kernelError);
+
+  # Validated through the dmsetup code
+  $kernelError = "too large device";
+  $bigNum = Math::BigInt->new(${maxUInt} * $KB);
+  $self->_tryIllegal("logicalSize",  $bigNum, $dmsetupError, $kernelError);
+  $bigNum = Math::BigInt->new(${maxUInt} * $MB);
+  $self->_tryIllegal("logicalSize",  $bigNum, $dmsetupError, $kernelError);
+  $bigNum = Math::BigInt->new(${maxUInt} * $GB);
+  $self->_tryIllegal("logicalSize",  $bigNum, $dmsetupError, $kernelError);
+  $bigNum = Math::BigInt->new(${maxUInt} * $PB);
+  $self->_tryIllegal("logicalSize",  $bigNum, $dmsetupError, $kernelError);
+  $bigNum = Math::BigInt->new(${maxUInt} * $TB);
+  $self->_tryIllegal("logicalSize",  $bigNum, $dmsetupError, $kernelError);
+
+  # TODO: Add subtests that expect to succeed when formatting code done
+}
+
+1;
