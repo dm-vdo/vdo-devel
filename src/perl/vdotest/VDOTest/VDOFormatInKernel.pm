20d4a929a807b3ecfb56f65c51c47b00d46fab5c
Bruce Johnston <bjohnsto@redhat.com>
2024-11-19 14:35:17 -0500

    dm vdo test: update perl test for in kernel formatting
    Add additional sub tests now that formatting actually works.
    
    Signed-off-by: Bruce Johnston <bjohnsto@redhat.com>

--- src/perl/vdotest/VDOTest/VDOFormatInKernel.pm
+++ src/perl/vdotest/VDOTest/VDOFormatInKernel.pm
@@ -71,6 +71,9 @@ sub testOptions {
   my $vdoDevice = $self->getDevice();
 
   # Validated through the VDO kernel code
+  $vdoDevice->formatVDO({ slabBits => 14 });
+  $vdoDevice->formatVDO({ slabBits => 17 });
+
   my $kernelError = "invalid slab size";
   my $dmsetupError = "Invalid argument";
   $self->_tryIllegal("slabBits",              -2, $dmsetupError, $kernelError);
@@ -79,6 +82,12 @@ sub testOptions {
   $self->_tryIllegal("slabBits",  ($maxUInt - 1), $dmsetupError, $kernelError);
   $self->_tryIllegal("slabBits",        $maxUInt, $dmsetupError, $kernelError);
   $self->_tryIllegal("slabBits", $oneMoreThanMax, $dmsetupError, $kernelError);
+
+  # Validated through the VDO kernel code
+  $vdoDevice->formatVDO({ logicalSize => 4096 });
+  $vdoDevice->formatVDO({ logicalSize => 4 * $MB });
+  $vdoDevice->formatVDO({ logicalSize => 1 * $GB });
+  $vdoDevice->formatVDO({ logicalSize => 4 * $TB });
 
   # Validated through the dmsetup code
   $kernelError = "zero-length target";
@@ -107,7 +116,85 @@ sub testOptions {
   $bigNum = Math::BigInt->new(${maxUInt} * $TB);
   $self->_tryIllegal("logicalSize",  $bigNum, $dmsetupError, $kernelError);
 
-  # TODO: Add subtests that expect to succeed when formatting code done
+  $vdoDevice->formatVDO({ albireoMem => .25 });
+  $vdoDevice->formatVDO({ albireoMem => .5 });
+  $vdoDevice->formatVDO({ albireoMem => 1 });
+
+  $dmsetupError = "No space left";
+  $kernelError = "Could not allocate";
+  $self->_tryIllegal("albireoMem", 255, $dmsetupError, $kernelError);
+
+  $vdoDevice->formatVDO({ albireoSparse => 0 });
+  $vdoDevice->formatVDO({ albireoSparse => 1 });
+}
+
+#############################################################################
+# Properties to make sure physical threads = slab count.
+##
+sub propertiesMinimumSize {
+  return (
+	  physicalThreadCount => 1,
+         );
+}
+
+########################################################################
+##
+sub testMinimumSize {
+  my ($self) = assertNumArgs(1, @_);
+
+  my $vdoDevice = $self->getDevice();
+  my $machine = $vdoDevice->getMachine();
+
+  # This will fail because 50GB is too too small for the given
+  # parameters. So we fail then figure out how much space we
+  # actually need and do some sizing to test things with that value
+  my $preformatCursor = $machine->getKernelJournalCursor();
+  eval {
+    $vdoDevice->formatVDO({ albireoMem => 2, slabBits => 23 });
+  };
+  assertEvalErrorMatches(qr| reload ioctl on .* failed: No space left|);
+
+  my $journal = $machine->getKernelJournalSince($preformatCursor);
+  assertRegexpMatches(qr/Could not allocate/, $journal);
+
+  my $msg = ($journal =~ qr/Minimum required size for VDO volume: ([0-9]+) bytes/);
+  assertDefined($msg);
+  my $physicalSize = $1;
+
+  my $storageDevice = $vdoDevice->getStorageDevice();
+
+  # We round up to physicalExtentSize when extending, so this is the best we
+  # can do in terms of checking if less space fails
+  my $lessThanNeeded
+    = $physicalSize - $storageDevice->{volumeGroup}->{physicalExtentSize} - 1;
+
+  $storageDevice->extend($lessThanNeeded);
+
+  my $extend1Cursor = $machine->getKernelJournalCursor();
+  eval {
+    $vdoDevice->formatVDO({ albireoMem => 2, slabBits => 23 });
+  };
+  assertEvalErrorMatches(qr| reload ioctl on .* failed: No space left|);
+  assertTrue($machine->searchKernelJournalSince($extend1Cursor, "Could not allocate"));
+
+  $storageDevice->extend($physicalSize);
+  $vdoDevice->formatVDO({ albireoMem => 2, slabBits => 23 });
+}
+
+########################################################################
+##
+sub testDirtyStorage {
+  my ($self) = assertNumArgs(1, @_);
+
+  my $vdoDevice = $self->getDevice();
+  my $storageDevice = $vdoDevice->getStorageDevice();
+  $storageDevice->ddWrite(
+                          if    => "/dev/urandom",
+                          count => $storageDevice->getSize() / $self->{blockSize},
+                          bs    => $self->{blockSize},
+                         );
+
+  $vdoDevice->formatVDO({ albireoMem => .25 });
 }
 
 1;
