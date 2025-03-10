9f606d34dd040c53e99a9fce4c34c1a0e8d785c1
Chung Chung <cchung@redhat.com>
2025-03-10 18:39:40 -0400

    Change RHEL_RELEASE_VERSION comparison to exclude RHE10
    RHEL10 does not have a us_to_ktime definition, we need to exclude RHEL10
    in VDO_USE_NEXT definition.
    
    Signed-off-by: Chung Chung <cchung@redhat.com>

--- src/c++/uds/src/uds/time-utils.h
+++ src/c++/uds/src/uds/time-utils.h
@@ -17,7 +17,7 @@
 #include <linux/version.h>
 #undef VDO_USE_NEXT
 #if defined(RHEL_RELEASE_CODE) && defined(RHEL_MINOR) && (RHEL_MINOR < 50)
-#if (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(10, 0))
+#if (RHEL_RELEASE_CODE >= RHEL_RELEASE_VERSION(11, 0))
 #define VDO_USE_NEXT
 #endif
 #else /* !RHEL_RELEASE_CODE */
