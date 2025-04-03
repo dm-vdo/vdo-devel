79de5fc108173728235f6b98a8234397005a8835
Bruce Johnston <bjohnsto@redhat.com>
2025-04-03 10:07:02 -0400

    dm vdo user: add support for uuid_gen
    Add user mode version of uuid_gen function.
    
    Signed-off-by: Bruce Johnston <bjohnsto@redhat.com>

--- src/c++/vdo/user/vdoConfig.c
+++ src/c++/vdo/user/vdoConfig.c
@@ -4,9 +4,9 @@
  * %LICENSE%
  */
 
-#include <uuid/uuid.h>
-
 #include "vdoConfig.h"
+
+#include <linux/uuid.h>
 
 #include "logger.h"
 #include "memory-alloc.h"
@@ -83,7 +83,7 @@ int formatVDO(const struct vdo_config   *config,
 {
   // Generate a uuid.
   uuid_t uuid;
-  uuid_generate(uuid);
+  uuid_gen(&uuid);
 
   return formatVDOWithNonce(config, indexConfig, layer, current_time_us(),
                             &uuid);
