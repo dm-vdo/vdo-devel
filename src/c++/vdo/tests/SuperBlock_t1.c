42ba454b3a8abf745d7079f7dd70d5696cc65df8
Matthew Sakai <msakai@redhat.com>
2025-04-24 20:59:55 -0400

    dm vdo test: add test for new super block behavior
    Add a new test that verifies vdo will load super block version 67.0
    and write it back out if upgrade is not needed. Also create a new
    VDOVersion pickle for super block version 67.1.
    
    Signed-off-by: Matthew Sakai <msakai@redhat.com>

--- src/c++/vdo/tests/SuperBlock_t1.c
+++ src/c++/vdo/tests/SuperBlock_t1.c
@@ -22,7 +22,8 @@
 enum {
   // By measurement.
   SUPER_BLOCK_PAYLOAD_SIZE = 418,
-  SUPER_BLOCK_SIZE =  VDO_ENCODED_HEADER_SIZE + SUPER_BLOCK_PAYLOAD_SIZE,
+  SUPER_BLOCK_SIZE = VDO_ENCODED_HEADER_SIZE + SUPER_BLOCK_PAYLOAD_SIZE,
+  HEADER_AND_COMPONENT_SIZE = VDO_ENCODED_HEADER_SIZE + VDO_COMPONENT_ENCODED_SIZE,
   CHECKSUM_OFFSET = SUPER_BLOCK_SIZE - sizeof(u32),
 };
 
@@ -40,18 +41,18 @@ static u8 EXPECTED_SUPERBLOCK_12_0_ENCODED_HEADER[] =
   };
 
 /**********************************************************************/
-static void testSuperBlock(void)
+static void testCurrentSuperBlock(void)
 {
-  // Test set-up will have formatted and started the vdo confirming that what the synchronous save
-  // has written is intelligible to the asynchronous load.
+  // Test set-up will have formatted and started the vdo confirming that what the 
+  // synchronous save has written is intelligible to the asynchronous load.
 
   // Check the header.
   char block[VDO_BLOCK_SIZE];
   VDO_ASSERT_SUCCESS(layer->reader(layer, getSuperBlockLocation(), 1, block));
   UDS_ASSERT_EQUAL_BYTES(EXPECTED_SUPERBLOCK_12_0_ENCODED_HEADER, block, VDO_ENCODED_HEADER_SIZE);
 
-  // Stop the VDO, confirm that the super block modified as the vdo state will have changed from
-  // VDO_NEW to VDO_CLEAN.
+  // Stop the VDO, confirm that the super block modified as the vdo state will
+  //  have changed from VDO_NEW to VDO_CLEAN.
   stopVDO();
   char loaded[VDO_BLOCK_SIZE];
   VDO_ASSERT_SUCCESS(layer->reader(layer, getSuperBlockLocation(), 1, loaded));
@@ -71,10 +72,55 @@ static void testSuperBlock(void)
 }
 
 /**********************************************************************/
+static void testSuperBlock67_0(void)
+{
+  // Copy the component states and reset them to look like the old version.
+  char oldBlock[VDO_BLOCK_SIZE];
+  struct vdo_component_states oldStates = vdo->states;
+  oldStates.volume_version = VDO_VOLUME_VERSION_67_0;
+  oldStates.required_flags = 0xabcd1234;
+  oldStates.legacy = 0x6701dead;
+  oldStates.vdo = vdo->states.vdo;
+  oldStates.vdo.state = VDO_CLEAN;
+  vdo_encode_super_block((u8 *) &oldBlock, &oldStates);
+
+  // Stop VDO, and replace the superblock with the older version.
+  stopVDO();
+  VDO_ASSERT_SUCCESS(layer->writer(layer, getSuperBlockLocation(), 1, oldBlock));
+
+  // Confirm that synchronous load can read the old super block format.
+  VDO_ASSERT_SUCCESS(vdo_decode_super_block((u8 *) &oldBlock));
+
+  // Start VDO and check that the proper fields were loaded.
+  startVDO(VDO_CLEAN);
+  CU_ASSERT(vdo_are_same_version(vdo->states.volume_version,
+                                 VDO_VOLUME_VERSION_67_0));
+  CU_ASSERT_EQUAL(vdo->states.legacy, 0x6701dead); 
+  CU_ASSERT_EQUAL(vdo->states.required_flags, VDO_REQUIRES_LZ4); 
+
+  // Stop VDO and check that header and component are the same.
+  stopVDO();
+  char loaded[VDO_BLOCK_SIZE];
+  VDO_ASSERT_SUCCESS(layer->reader(layer, getSuperBlockLocation(), 1, loaded));
+  VDO_ASSERT_SUCCESS(vdo_decode_super_block((u8 *) &loaded));
+  // Other super block data will be updated so we can't compare the entire block.
+  UDS_ASSERT_EQUAL_BYTES(oldBlock, loaded, HEADER_AND_COMPONENT_SIZE);
+    
+  // Break the checksum and confirm that decode/load fails.
+  u8 checksumByte = loaded[CHECKSUM_OFFSET];
+  memset(loaded + CHECKSUM_OFFSET, ++checksumByte, 1);
+  CU_ASSERT_EQUAL(VDO_CHECKSUM_MISMATCH, vdo_decode_super_block((u8 *) &loaded));
+  VDO_ASSERT_SUCCESS(layer->writer(layer, getSuperBlockLocation(), 1, loaded));
+  setStartStopExpectation(-EIO);
+  startAsyncLayer(getTestConfig(), true);
+}
+
+/**********************************************************************/
 
 static CU_TestInfo superBlockTests[] = {
-  { "test super block save and load", testSuperBlock  },
-  CU_TEST_INFO_NULL
+  { "test current super block save and load", testCurrentSuperBlock },
+  { "test super block v67.0 save and load",   testSuperBlock67_0 },
+  CU_TEST_INFO_NULL,
 };
 
 static CU_SuiteInfo superBlockSuite = {
