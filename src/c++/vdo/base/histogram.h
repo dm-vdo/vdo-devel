844effa3661eafa65a0f497b94e9bd805b25be60
Bruce Johnston <bjohnsto@redhat.com>
2025-06-06 17:24:53 -0400

    dm vdo tests: Remove vdo/histogram sysfs fields
    Convert histogram sysfs fields into a dmsetup message string
    
    This commit:
    - Adds 'dmsetup message histograms' to output all histogram info
      into a JSON string
    - Adds 'dmsetup message histogram limit' to set histogram limits
    - Removes all sysfs references from vdo-histograms.[ch] and
      histogram.[ch]
    - Adds name field to the histogram structure. The sysfs supported
      a name automatically before
    - Adds histogram_is_named() function to compare a histogram's name
      against a passed in parameter to be used for limit setting
    - Adds write_histogram() function to print a histogram's info (as
      a JSON string) into a buffer
    - Adds histogram_write_item() function to print a value into a
      buffer and then updates the buffer pointer and how much space
      the buffer has left to print to. This will be used in all output
      functions to create a JSON string
    - Adds histogram_write_striing() function to print a simple string
      into a buffer by calling histogram_write_item()
    - Reworks histogram_show_histogram(). This function no longer prints
      bars or other sort of graphical like objects. As this is a JSON
      string, the output will be an array of bucket ranges and then how
      many samples were in each particular bucket. The client (our
      VDO.pm perl code) will print the graphics
    - Adds several new show functions that output fields that used to
      be part of histogram_show_histogram(). Now that the code was
      reworked, we're adding these fields to the JSON so they are
      available to clients
    - Updates free_histogram to still free objects now that sysfs is
      gone
    - Updates VDO.pm to parse new dmsetup message output and print
      out the same histogram info the logHistograms() function
      previously did
    
    Signed-off-by: Bruce Johnston <bjohnsto@redhat.com>

--- src/c++/vdo/base/histogram.h
+++ src/c++/vdo/base/histogram.h
@@ -8,23 +8,37 @@
 
 #include <linux/types.h>
 
-struct histogram *make_linear_histogram(struct kobject *parent, const char *name,
+struct histogram *make_linear_histogram(const char *name,
 					const char *init_label,
 					const char *counted_items, const char *metric,
 					const char *sample_units, int size);
 
-struct histogram *make_logarithmic_histogram(struct kobject *parent, const char *name,
+struct histogram *make_logarithmic_histogram(const char *name,
 					     const char *init_label,
 					     const char *counted_items,
 					     const char *metric,
 					     const char *sample_units, int log_size);
 
-struct histogram *make_logarithmic_jiffies_histogram(struct kobject *parent, const char *name,
+struct histogram *make_logarithmic_jiffies_histogram(const char *name,
 						     const char *init_label,
 						     const char *counted_items,
 						     const char *metric, int log_size);
 
 void enter_histogram_sample(struct histogram *h, u64 sample);
+
+void __printf(3, 4) histogram_write_item(char **buffer, unsigned int *maxlen,
+					 const char *format, ...);
+
+static inline void histogram_write_string(char **buffer, unsigned int *maxlen, char *string)
+{
+	histogram_write_item(buffer, maxlen, "%s", string);
+}
+
+void write_histogram(struct histogram *histogram, char **buf, unsigned int *maxlen);
+
+bool histogram_is_named(struct histogram *histogram, const char *name);
+
+ssize_t histogram_store_limit(struct histogram *h, const char *buf, size_t length);
 
 void free_histogram(struct histogram *histogram);
 
