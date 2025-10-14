dm-vdo/vdo-devel

commit 92a7689ef6b950eb1df755b27c5ada6da8df50a1

dm vdo overlay: reduce makefile dependencies
Integrate overlay and kpatch steps into the overlay script itself,
to remove most complications of using the overlay script with a
mismatched Makefile.

Signed-off-by: Matthew Sakai <msakai@redhat.com>

src/packaging/kpatch/commit_to_overlay.sh
@@ -364,7 +364,7 @@ _cleanup() {
     rm -fv ${commit_file}
   fi
 
-  unset -f COMMIT_FILE DEBUG GITDATE GITAUTHOR LINUX_SRC
+  unset -f COMMIT_FILE DEBUG LINUX_SRC
 }
 
 ###############################################################################
@@ -386,6 +386,11 @@ VDO_TREE=${RUN_DIR}/$(mktemp -d vdo-raw-XXXXXX)
 # Shallow clone the specified dm-linux repo branch into LINUX_SRC
 echo "Cloning kernel repo $(get_repo_name ${KERNEL_REPO_URL})/${KERNEL_BRANCH}"
 git clone --branch ${KERNEL_BRANCH} --depth 1 ${KERNEL_REPO_URL} ${LINUX_SRC}
+
+LINUX_MD_SRC=${LINUX_SRC}/drivers/md
+LINUX_VDO_SRC=${LINUX_MD_SRC}/dm-vdo
+LINUX_DOC_SRC=${LINUX_SRC}/Documentation/admin-guide/device-mapper
+
 cd $LINUX_SRC
 git checkout -b ${OVERLAY_BRANCH} ${KERNEL_BRANCH}
 
@@ -424,8 +429,8 @@ for change in ${COMMIT_SHAS[@]}; do
   commit_author_email=$(git log --format=%aE -1 ${change})
   commit_author_name=$(git log --format=%aN -1 ${change})
   commit_date=$(git log --format=%ad -1 ${change})
-  gitauthor="--author \"${commit_author_name} <${commit_author_email}>\""
-  gitdate="--date \"${commit_date}\""
+  gitauthor="--author=\"${commit_author_name} <${commit_author_email}>\""
+  gitdate="--date=\"${commit_date}\""
 
   # Build the necessary parts of the tree
   build_log_file=$(mktemp /tmp/overlay_build_log.XXXXX)
@@ -445,27 +450,36 @@ for change in ${COMMIT_SHAS[@]}; do
   echo "Generating the kernel overlay"
   cd src/packaging/kpatch
 
-  export GITDATE="${gitdate}" GITAUTHOR="${gitauthor}" COMMIT_FILE="${commit_file}" \
-         LINUX_SRC=${LINUX_SRC}
-  make prepare kernel-overlay >> ${build_log_file}
-
+  export LINUX_SRC=${LINUX_SRC}
+  make prepare >> ${build_log_file}
   if [[ $? != 0 ]]; then
-    echo -e "${COLOR_RED}ERROR: Generating the kernel overlay failed. See ${build_log_file} for" \
+    echo -e "${COLOR_RED}ERROR: Preparing kernel files failed. See ${build_log_file} for" \
             "more information.${NO_COLOR}"
     exit
   fi
 
-  echo "Applying the kernel overlay to branch '${OVERLAY_BRANCH}'"
-  make kernel-kpatch >> ${build_log_file} 2>&1
-
+  WORK_DIR=work/kvdo-* # Location of the prepared kernel products
+  rm -rf ${LINUX_VDO_SRC} && \
+  mkdir -p ${LINUX_VDO_SRC} && \
+  cp -r ${WORK_DIR}/dm-vdo/* ${LINUX_VDO_SRC} && \
+  cp -f ${WORK_DIR}/vdo.rst ${WORK_DIR}/vdo-design.rst ${LINUX_DOC_SRC}/
   if [[ $? != 0 ]]; then
-    return=$(tail -5 ${build_log_file} | grep "^[nN]othing to commit" | wc -l)
+    echo -e "${COLOR_RED}ERROR: Generating the kernel overlay failed.${NO_COLOR}"
+    exit
+  fi
 
-    if [[ $return > 0 ]]; then
-      echo "Nothing to apply - moving on."
+  echo "Applying the kernel overlay to branch '${OVERLAY_BRANCH}'"
+  git -C ${LINUX_SRC} diff-index --cached --quiet HEAD
+  if [[ $? == 0 ]]; then
+    echo "Nothing to apply - moving on."
+  else
+    git -C ${LINUX_SRC} add . && \
+    git -C ${LINUX_SRC} commit -s "${gitauthor}" "${gitdate}" --file "${commit_file}"
+    if [[ $? == 0 ]]; then
+      echo "Committed"
     else
-      echo -e "${COLOR_RED}ERROR: Applying the kernel overlay to branch '${OVERLAY_BRANCH}'" \
-              "failed. See ${build_log_file} for more information.${NO_COLOR}"
+        echo -e "${COLOR_RED}ERROR: Applying the kernel overlay to branch" \
+                "'${OVERLAY_BRANCH}' failed."
       exit
     fi
   fi
@@ -479,7 +493,7 @@ for change in ${COMMIT_SHAS[@]}; do
     rm -fv ${build_log_file}
   fi
 
-  unset -f GITDATE GITAUTHOR COMMIT_FILE
+  unset -f COMMIT_FILE
 done
 
 # Push the kernel overlay branch to the remote kernel repository

src/packaging/kpatch/Makefile
@@ -71,8 +71,6 @@ endif
 
 GIT=git
 GITARGS=
-GITAUTHOR ?= ""
-GITDATE ?= ""
 
 WORK_DIR ?= $(realpath .)/work
 PREPARED_DIR = $(WORK_DIR)/kvdo-$(VDO_VERSION)/dm-vdo
@@ -206,14 +204,6 @@ overlay overlay_vdo:
 
 	cp -f $(VDO_DOC) $(LINUX_DOC_SRC)/
 
-.PHONY: kernel-overlay
-kernel-overlay:
-	rm -rf $(LINUX_VDO_SRC)
-	mkdir -p $(LINUX_VDO_SRC)
-	cp -r $(PREPARED_DIR)/* $(LINUX_VDO_SRC)
-
-	cp -f $(VDO_DOC) $(LINUX_DOC_SRC)/
-
 #
 # The following are git operations that work on the linux source tree
 # in $(LINUX_SRC).
@@ -237,12 +227,6 @@ kpatch:
 	cd $(LINUX_SRC) && $(GIT) $(GITARGS) add . \
 	  && $(GIT) $(GITARGS) commit -m "$(CHANGE_LOG)" \
 	  && $(GIT) $(GITARGS) format-patch -s -o .. HEAD ^origin/$(LINUX_BRANCH)
-
-.PHONY: kernel-kpatch
-kernel-kpatch:
-	cd $(LINUX_SRC) && $(GIT) $(GITARGS) add . \
-	  && $(GIT) $(GITARGS) commit $(GITAUTHOR) $(GITDATE) --file "$(COMMIT_FILE)" \
-	  && echo committed
 
 # Parallel builds are risky since all of the targets here are a linear
 # pipeline.

commit d0a85ec3ec874dee0095c3ebbf75bfaec6d21712

dm vdo overlay: compare unstaged diffs
This is necessary because the overlay diffs haven't been added to
the index yet, so the script never sees any changes to commit.
This fixes a bug introduced by 92a7689ef6b9 ("dm vdo overlay:
reduce makefile dependencies").

Also fix an indentation issue.

Signed-off-by: Matthew Sakai <msakai@redhat.com>

src/packaging/kpatch/commit_to_overlay.sh
@@ -463,7 +463,7 @@ for change in ${COMMIT_SHAS[@]}; do
   fi
 
   echo "Applying the kernel overlay to branch '${OVERLAY_BRANCH}'"
-  git -C ${LINUX_SRC} diff-index --cached --quiet HEAD
+  git -C ${LINUX_SRC} diff --quiet HEAD
   if [[ $? == 0 ]]; then
     echo "Nothing to apply - moving on."
   else
@@ -472,8 +472,8 @@ for change in ${COMMIT_SHAS[@]}; do
     if [[ $? == 0 ]]; then
       echo "Committed"
     else
-        echo -e "${COLOR_RED}ERROR: Applying the kernel overlay to branch" \
-                "'${OVERLAY_BRANCH}' failed."
+      echo -e "${COLOR_RED}ERROR: Applying the kernel overlay to branch" \
+              "'${OVERLAY_BRANCH}' failed."
       exit
     fi
   fi